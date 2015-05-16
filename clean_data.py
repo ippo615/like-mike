0e5a2e15552de9031e2cd4880198fe70c61206bc
Anna <anna.nicanorova@annalect.com>
2015-05-16 15:43:53 -0400

    first update. draft of data cleaning

---
+++ clean_data.py
@@ -0,0 +1,60 @@
+import json 
+import csv
+import os
+import pandas as pd
+import re
+import sys
+
+
+os.chdir('Stats.data/stats/tennis/wta')
+
+f = open('sample.json') 
+data = json.load(f) 
+f.close()
+f = open('data.csv') 
+# csv_file = csv.writer(f) 
+# for item in data: 
+#     f.writerow(item) 
+# f.close()
+
+# data.keys()
+
+# try:
+# 	del data['this']
+# except KeyError:
+# 	pass
+
+# for key in data:
+# 	print key
+
+test = data['with']
+
+test2 = pd.DataFrame(test)
+
+# df2 = test2['content']
+# df = pd.concat(df2, axis=1)
+df = pd.DataFrame(test2)
+
+df['tilt_z'] = ''
+df['tilt_x'] = ''
+df['tilt_y'] = ''
+
+for rownum, row in df.iterrows():
+	# print row
+	tilt_z = re.findall(r'tilt_z\': (\d*)', str(row))
+	df.loc[rownum, 'tilt_z']= tilt_z
+	tilt_x = re.findall(r'tilt_x\': (\d*)', str(row))
+	df.loc[rownum, 'tilt_x']= tilt_x
+	tilt_y = re.findall(r'tilt_y\': (\d*)', str(row))
+	df.loc[rownum, 'tilt_y']= tilt_x
+
+
+# type(df['tilt_z'][4])
+		#print type(tilt_z[0])
+	# try:
+	# 	tilt_z = float(tilt_z[0])
+	# 	print tilt_z
+	# 	df.loc[rownum, 'tilt_z']= tilt_z
+	# except: # catch *all* exceptions
+	# 	e = sys.exc_info()[0]
+	# 	write_to_page( "<p>Error: %s</p>" % e )
