35e7232593d9ccc534914f0a61c38e4cabecc0d2
Andrew Ippoliti <acippolit@gmail.com>
2015-05-17 09:02:36 -0400

    Added testing round data

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 import sys
 import threading
 import subprocess
+import time
 
 import dweepy
 
@@ -51,7 +52,7 @@ def push_score( thing, score, text ):
 
 def one_round(thing='default-thing'):
 	score = 0
-	push_score( thing, score )
+	push_score( thing, score, '' )
 	samples = []
 	index = 0
 	for dweet in dweepy.listen_for_dweets_from(thing):
@@ -75,6 +76,23 @@ def one_round(thing='default-thing'):
 	#	tilt
 	#)
 
+def test_round(thing='default-thing'):
+	push_score( thing, 2, 'Just getting warmed up' )
+	time.sleep(8)
+	push_score( thing, 4, 'You could use some practice' )
+	time.sleep(4)
+	push_score( thing, 8, 'You are getting there' )
+	time.sleep(6)
+	push_score( thing, 1, 'Try harder' )
+	time.sleep(4)
+	push_score( thing, 8, 'You are getting there' )
+	time.sleep(7)
+	push_score( thing, 10, 'You rock like Mike' )
+	time.sleep(5)
+	push_score( thing, 12, 'OMG!!! You have outperformed Mike' )
+	time.sleep(3)
+	push_score( thing, 10, 'You rock like Mike' )
+
 def simple_test(thing):
 	return compute_score( None, 'data-set2.csv', 'ill-fated-anger', 'x' )
 
@@ -84,11 +102,12 @@ def blah_test():
 if __name__ == '__main__':
 	# Wait for a message
 	print 'Waiting for start...'
-	for dweet in dweepy.listen_for_dweets_from('si-hacks-2015-05-16-blah-start'):
+	for dweet in dweepy.listen_for_dweets_from('si-hacks-2015-05-16-blah-start', timeout=10):
 		print dweet
 		try:
 			runner = threading.Thread(
-				target=one_round,
+				#target=one_round,
+				target=test_round,
 				kwargs=dict(
 					thing=dweet['content']['thing']
 				)
